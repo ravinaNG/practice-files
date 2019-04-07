c899c5825078305d4f59cb370734a93ee0ce9dc4
ravinaNG <40683703+ravinaNG@users.noreply.github.com>
2019-04-07 08:01:22 +0530

    Update Reverse_str.py

--- Reverse_str.py
+++ Reverse_str.py
@@ -1,7 +1,7 @@
 user = raw_input("Type any string:- ")
 string = ""
 count = -1
-while(count < -(len(user))):
+while(count >= -(len(user))):
   string = string + user[count]
   count = count - 1
 print string
